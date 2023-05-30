1747757e9413d91d36908cfa44be1a9487dc8c36
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-05-29 18:36:54 -0600

    final commit

--- server/app.py
+++ server/app.py
@@ -17,6 +17,26 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
+class Login(Resource):
+
+    def post(self):
+        user = User.query.filter(User.username == request.get_json()['username']).first()
+        session['user_id'] = user.id
+        return user.to_dict(), 200
+
+class Logout(Resource):
+
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
+    
+class CheckSession(Resource):
+
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            return user.to_dict(), 200
+        return {}, 401
 
 class ClearSession(Resource):
 
@@ -48,6 +68,9 @@ class ShowArticle(Resource):
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
